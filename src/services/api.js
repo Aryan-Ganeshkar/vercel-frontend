9994ca2d8598d67076fef2c0bacd2870eb01c9cb
Aryan-Ganeshkar <ganeshkararyan@gmail.com>
2025-03-20 12:51:40 +0530

    backend link add

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/companies";
+const API_URL = "https://vercel-backend-three-ashen.vercel.app//api/companies";
 
 export const fetchCompanies = () => axios.get(API_URL);
 export const addCompany = (company) => axios.post(API_URL, company);
